11df2417bce88275073d60fb2de31379b7e12a76
kimmyoungsoo <kimmyoungsoo@kimmyoungsoos-MacBook-Air.local>
2026-06-07 17:01:40 +0900

    configure

--- lecture/k번째약수.py
+++ lecture/k번째약수.py
@@ -1,4 +1,12 @@
-import sys
-sys.stdin=open("./lecture/input.txt","rt")
-n=input()
-print(n)
+n, k=map(int,input().split())
+
+answer = []
+
+for i in range(1,n+1) :
+    if n % i == 0 :
+        answer.append(i)
+
+if len(answer) < k :
+    print(-1)
+else :
+    print(answer[k-1])
